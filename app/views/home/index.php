5b6f38bef57e0dc05df133f579948b56e3c02684
franzxml <ignfranzx@gmail.com>
2025-12-17 01:33:02 +0700

    chore: clear main content from home page view

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,20 +1,6 @@
-<?php
-/**
- * Home Index View.
- *
- * Uses the global header and footer partials.
- * @var array $data Passed from controller.
- */
-require APPROOT . '/views/inc/header.php';
-?>
+<?php require APPROOT . '/views/inc/header.php'; ?>
 
-<div class="container text-center">
-    <div class="hero-section">
-        <h1 class="display-title"><?php echo $data['title']; ?></h1>
-        <p class="lead-text"><?php echo $data['description']; ?></p>
-        <hr class="divider">
-        <p>Manage your Genshin Impact character database efficiently.</p>
+<div class="container">
     </div>
-</div>
 
 <?php require APPROOT . '/views/inc/footer.php'; ?>
