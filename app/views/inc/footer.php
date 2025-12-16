e5257eb156d5850d744b411e15d69bba085cb5f3
franzxml <ignfranzx@gmail.com>
2025-12-16 19:56:41 +0700

    rework

--- app/views/inc/footer.php
+++
@@ -1,7 +0,0 @@
-</div> <footer style="margin-top: 50px; text-align: center;">
-        <p>Genpedia &copy; <?php echo date('Y'); ?></p>
-    </footer>
-
-    <script src="<?php echo URLROOT; ?>/assets/js/main.js"></script>
-</body>
-</html>
