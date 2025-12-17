19f2b1b9b6af0e080af29882ffe3948ecf258a98
franzxml <ignfranzx@gmail.com>
2025-12-17 14:42:28 +0700

    refactor: remove welcome text, display white page only

--- app/views/home.php
+++ app/views/home.php
@@ -7,8 +7,5 @@
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/base.css">
 </head>
 <body>
-    <div class="container">
-        <h1>Welcome to Genpedia</h1>
-    </div>
 </body>
 </html>
