596cd783cd018e28b12ab992f569a430e7cb91da
franzxml <ignfranzx@gmail.com>
2025-12-17 15:16:25 +0700

    feat: add rounded rectangle hero section with #F4F5F6 background to homepage

--- app/views/home.php
+++ app/views/home.php
@@ -8,12 +8,15 @@
     <link rel="stylesheet" href="/css/components/header.css">
     <link rel="stylesheet" href="/css/components/navigation.css">
     <link rel="stylesheet" href="/css/components/home-content.css">
+    <link rel="stylesheet" href="/css/components/hero-section.css">
     <link rel="stylesheet" href="/css/components/footer.css">
 </head>
 <body>
     <?php include BASE_PATH . '/app/views/components/header.php'; ?>
     
     <main class="home-content">
+        <section class="hero-section">
+        </section>
     </main>
 
     <?php include BASE_PATH . '/app/views/components/footer.php'; ?>
