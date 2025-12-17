4a737022cabebee837f5dcd176788e8fa0acb755
franzxml <ignfranzx@gmail.com>
2025-12-17 14:47:52 +0700

    feat: add centered welcome title to homepage content

--- app/views/home.php
+++ app/views/home.php
@@ -6,8 +6,13 @@
     <title><?php echo APP_NAME; ?></title>
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/base.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/components/header.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>public/css/components/home-content.css">
 </head>
 <body>
     <?php include BASE_PATH . '/app/views/components/header.php'; ?>
+    
+    <main class="home-content">
+        <h2 class="home-title">Welcome to Genpedia!</h2>
+    </main>
 </body>
 </html>
