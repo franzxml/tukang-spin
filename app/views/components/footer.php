909deaa05187628b9533eb6b53883211a3c52e1d
franzxml <ignfranzx@gmail.com>
2025-12-17 15:33:18 +0700

    feat: update footer with developer credit and social media links

--- app/views/components/footer.php
+++ app/views/components/footer.php
@@ -8,6 +8,14 @@
 ?>
 <footer class="footer">
     <div class="footer-container">
-        <p class="footer-text">&copy; 2024 <?php echo APP_NAME; ?>. All rights reserved.</p>
+        <p class="footer-text">Developed by @franzxml</p>
+        <div class="footer-social">
+            <a href="https://github.com/franzxml" target="_blank" rel="noopener noreferrer" class="footer-social-link">
+                <img src="/images/icons/github.svg" alt="GitHub" class="footer-social-icon">
+            </a>
+            <a href="https://instagram.com/franzxml" target="_blank" rel="noopener noreferrer" class="footer-social-link">
+                <img src="/images/icons/instagram.svg" alt="Instagram" class="footer-social-icon">
+            </a>
+        </div>
     </div>
 </footer>
